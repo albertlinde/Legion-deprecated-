1ae071e25c1923c8ac15ba4cb37a371563e7736d
albertlinde <albert.linde@gmail.com>
2016-01-26 17:00:47 +0000

    runserver change

--- run-server.sh
+++ run-server.sh
@@ -7,4 +7,4 @@ cd applications
 node HTTPserver.js > ./../run/HTTPserver.log 2> ./../run/HTTPserver.error.log &
 
 node ./../framework/server/Signallingserver.js > ./../run/SignallingServer.log 2> ./../run/SignallingServer.error.log &
-#node ./../framework/server/ObjectsServer.js > ./../run/ObjectsServer.log 2> ./../run/ObjectsServer.error.log &
+node ./../framework/server/ObjectsServer.js > ./../run/ObjectsServer.log 2> ./../run/ObjectsServer.error.log &
