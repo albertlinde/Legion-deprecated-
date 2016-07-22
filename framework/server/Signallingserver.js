6c808cba5581d2f9bbafabe55404eaf94aac0e94
albertlinde <albert.linde@gmail.com>
2016-07-22 11:58:06 +0100

    Signalling server major cleanup and start on config file.

--- framework/server/Signallingserver.js
+++ framework/server/Signallingserver.js
@@ -1,35 +1,28 @@
-var PORT = 8002;
-
-var SERVER_HB_INTERVAL = 25000;
-var SERVER_HB_VALIDITY = 75000;
-
-var D = require('./../shared/Duplicates.js');
+var Config = require('./config.js');
+var Duplicates = require('./../shared/Duplicates.js').Duplicates;
 
 var util = require('util');
 var WebSocket = require('ws');
-var storage = require('node-persist');
-
-var WebSocketServer;
-var wss;
 
 var ALMap = require('./../shared/ALMap.js').ALMap;
 var AuthServer = require('./Authserver.js').AuthServer;
 
 initService();
+
+var WebSocketServer;
+var wss;
 function initService() {
     WebSocketServer = WebSocket.Server;
     wss = new WebSocketServer({
-        port: PORT, verifyClient: function (info, cb) {
+        port: Config.signalling.PORT, verifyClient: function (info, cb) {
             cb(true);
         }
     });
 
-    var duplicates = new D.Duplicates();
-    var nodes = new ALMap();
-    var iterableNodes = [];
+    var duplicates = new Duplicates();
+
     var messageCount = 0;
 
-    //TODO: id is hardcoded.
     //TODO: on server re-boot message generation breaks.
     /**
      *
@@ -39,39 +32,36 @@ function initService() {
     var generateMessage = function (type) {
         return {
             type: type,
-            sender: "SignallingServer",
+            sender: Config.signalling.SENDER_ID,
             ID: ++messageCount
         };
     };
 
-    /**
-     *
-     * @param sourceID {number}
-     * @param TargetID {number}
-     * @returns {number}
-     */
-    var computeDistance = function (sourceID, TargetID) {
-        //TODO: usage?
-        return 1;
-    };
-
+    var nodes = new NodesStructure();
     var authority = new AuthServer();
 
     //TODO: well defined security and parameters
     setInterval(function () {
         var HB = generateMessage("SHB");
         HB.timestamp = Date.now();
-        HB.validity = SERVER_HB_VALIDITY;
+        HB.validity = Config.signalling.SERVER_HB_VALIDITY;
         HB.KeyID = authority.getCurrentKey().id;
         HB.signature = authority.signedMessageDigest("" + HB.timestamp + HB.ID + HB.KeyID + HB.validity);
         var msg = JSON.stringify(HB);
-        for (var i = 0; i < iterableNodes.length; i++) {
-            if (iterableNodes[i].readyState == 1) {
-                util.log("-Sending HB to " + iterableNodes[i].remoteID);
-                iterableNodes[i].send(msg);
+
+        var deadNodes = [];
+        for (var i = 0; i < nodes.size(); i++) {
+            var node = nodes.getNodeByPos(i);
+            if (node.readyState == 1) {
+                util.log("-Sending HB to " + node.remoteID);
+                node.send(msg);
+            } else {
+                deadNodes.push(nodes.getIDByPos(i));
             }
         }
-    }, SERVER_HB_INTERVAL);
+        //TODO: remove the deadNodes.
+
+    }, Config.signalling.SERVER_HB_INTERVAL);
 
     wss.on('connection', function (socket) {
             util.log("Connection.");
@@ -85,107 +75,56 @@ function initService() {
                         socket.send(JSON.stringify(auth));
                         if (auth.auth.result == "Failed") {
                             socket.close();
+                        } else {
+                            //TODO: change adding of node to here.
                         }
                     } else {
                         if (!duplicates.contains(parsed.sender, parsed.ID)) {
                             duplicates.add(parsed.sender, parsed.ID);
                             if (!socket.remoteID) {
-                                if (nodes.contains(parsed.sender)) {
+                                if (nodes.containsByID(parsed.sender)) {
                                     util.log("Reconnected with new socket " + parsed.sender);
                                 } else {
                                     util.log("Connected " + parsed.sender);
                                 }
                                 socket.remoteID = parsed.sender;
-                                nodes.set(parsed.sender, socket);
-                                iterableNodes.push(socket);
+                                nodes.addNode(parsed.sender, socket);
                             }
 
                             if (parsed.destination != null) {
                                 //Try to send do destination, on failure back to default behaviour.
-                                var node = nodes.get(parsed.destination);
+                                var node = nodes.getNodeByID(parsed.destination);
                                 if (node && node.readyState == 1) {
                                     util.log("Destination message: " + parsed.type);
                                     util.log("   Sending to " + parsed.destination);
-                                    nodes.get(parsed.destination).send(message);
+                                    node.send(message);
                                     return;
                                 }
                             }
-                            if (parsed.type == "GetClosestContact") {
-                                util.log("GetClosestContact: " + parsed.targetID + " - " + parsed.sender);
-                                var targetID = parsed.targetID;
-                                var ids = nodes.keys();
-                                var closestDistance = null;
-                                var closestIdentifier = null;
-                                for (var i = 0; i < ids.length; i++) {
-                                    if (nodes.get(ids[i]) === socket) continue;
-                                    if (nodes.get(ids[i]).readyState == 1 && (closestDistance == null || computeDistance(targetID, ids[i].remoteID) < closestDistance)) {
-                                        closestDistance = computeDistance(targetID, ids[i].remoteID);
-                                        closestIdentifier = ids[i];
-                                    }
-                                }
-                                util.log("Value of closestIdentifier: " + closestIdentifier);
-                                var reply = generateMessage("GetClosestContactReply");
-                                reply.destination = parsed.sender;
-                                if (closestIdentifier != null) {
-                                    //util.log("   Sending to " + closestIdentifier);
-                                    //nodes.get(closestIdentifier).send(message);
-                                    //Notify the new node that its connection request has been forwarded to someone.
-                                    reply.contactID = closestIdentifier;
-                                    util.log("   Sending to " + socket.remoteID);
-                                    socket.send(JSON.stringify(reply));
-                                } else {
-                                    //Notify the new node that he is the first one arriving.
-                                    reply.contactID = null;
-                                    util.log("   Sending to " + socket.remoteID);
-                                    socket.send(JSON.stringify(reply));
-                                }
-
-                            } else if (parsed.howMany) {
-                                util.log("Sending howMany " + parsed.type + " - " + parsed.howMany);
-                                var sent = 0;
-                                var i = Math.floor(Math.random() * iterableNodes.length);
-                                for (var count = 0; sent < parsed.howMany && count < iterableNodes.length; count++) {
-                                    if (iterableNodes[i].readyState == 1 && !(iterableNodes[i] === socket)) {
-                                        if (iterableNodes[i].remoteID == parsed.sender)continue;
-                                        util.log("   Sending to " + iterableNodes[i].remoteID);
-                                        sent++;
-                                        parsed.destination = iterableNodes[i].remoteID;
-                                        iterableNodes[i].send(JSON.stringify(parsed));
-                                    }
-                                    i++;
-                                    if (i >= iterableNodes.length) i = 0;
-                                }
-                            } else {
-                                util.log("Broadcast: " + parsed.type);
-                                var end = iterableNodes.length;
-                                if (parsed.N) {
-                                    end = Math.min(parsed.N, end);
-                                }
-                                if (parsed.ttl) {
-                                    parsed.ttl--;
-                                }
-
-                                for (var i = 0; i < end; i++) {
-                                    if (iterableNodes[i] === socket)continue;
-                                    if (iterableNodes[i].remoteID == parsed.sender)continue;
-                                    if (iterableNodes[i].readyState == 1) {
-                                        util.log("   Sending to " + iterableNodes[i].remoteID);
-                                        iterableNodes[i].send(message);
-                                    }
-                                }
+                            util.log("Broadcast: " + parsed.type);
+                            var end = nodes.size();
+                            if (parsed.N) {
+                                end = Math.min(parsed.N, end);
+                            }
+                            if (parsed.ttl) {
+                                parsed.ttl--;
                             }
 
-                            var keys = nodes.keys();
-                            iterableNodes = [];
-                            for (var i = 0; i < keys.length; i++) {
-                                if (nodes.get(keys[i]).readyState == 1) {
-                                    iterableNodes.push(nodes.get(keys[i]));
+                            var deadNodes = [];
+                            for (var i = 0; i < end; i++) {
+                                //TODO: if sending to N, randomize the nodes it is sent to.
+                                var node = nodes.getNodeByPos(i);
+                                if (node === socket)continue;
+                                if (node.remoteID == parsed.sender)continue;
+
+                                if (node.readyState == 1) {
+                                    util.log("   Sending to " + node.remoteID);
+                                    node.send(message);
                                 } else {
-                                    util.log("Disconnected " + keys[i]);
-                                    nodes.delete(keys[i]);
+                                    deadNodes.push(nodes.getIDByPos(i));
                                 }
                             }
-                            iterableNodes = shuffle(iterableNodes);
+                            //TODO: remove the deadNodes.
                         }
                     }
                 }
@@ -197,19 +136,47 @@ function initService() {
     )
 }
 
-/**
- *
- * Copypasta from http://stackoverflow.com/a/962890
- * @param array {Array.<Object>}
- * @returns {Array.<Object>}
- */
-var shuffle = function (array) {
-    var tmp, current, top = array.length;
-    if (top) while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-    }
-    return array;
+function NodesStructure() {
+    this.nodesArray = [];
+    this.nodesMap = [];
+
+    this.arrayPosID = 0;
+    this.arrayPosSocket = 1;
+}
+
+NodesStructure.prototype.size = function () {
+    return this.nodesArray.length;
+};
+
+NodesStructure.prototype.getNodeByPos = function (pos) {
+    return this.nodesArray[pos][this.arrayPosSocket];
+};
+
+NodesStructure.prototype.containsByID = function (id) {
+    return this.nodesMap[id] != null;
+};
+
+NodesStructure.prototype.getIDByPos = function (pos) {
+    return this.nodesArray[pos][this.arrayPosID];
+};
+
+NodesStructure.prototype.getNodeByID = function (id) {
+    return this.nodesArray[this.nodesMap[id]][this.arrayPosSocket];
+};
+
+NodesStructure.prototype.addNode = function (id, socket) {
+    this.nodesArray.push([id, socket]);
+    this.nodesMap[id] = this.nodesArray.length - 1;
+};
+
+NodesStructure.prototype.removeNodeByID = function (id) {
+    var pos = this.nodesMap[id];
+    delete this.nodesMap[id];
+    this.nodesArray = this.nodesArray.splice(pos, 1);
+};
+
+NodesStructure.prototype.removeNodeByPos = function (pos) {
+    var id = this.nodesArray[pos][this.arrayPosID];
+    delete this.nodesMap[id];
+    this.nodesArray = this.nodesArray.splice(pos, 1);
 };
---
+++ framework/server/config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.signalling = {
+    PORT: 8002,
+    SERVER_HB_INTERVAL: 25000,
+    SERVER_HB_VALIDITY: 75000,
+    SENDER_ID: "SignallingServer"
+};
+
+module.exports = config;
